1b4178c341f283c5379050a559f483fd2e4ea348
alex bravp <fabriciobravo373@gmail.com>
2026-01-28 11:59:24 -0500

    pipe

--- ManejoArchivos/src/lectura01/PruebaLeerArchivoTexto.java
+++ ManejoArchivos/src/lectura01/PruebaLeerArchivoTexto.java
@@ -5,7 +5,7 @@ public class PruebaLeerArchivoTexto {
 
     public static void main(String args[]) {
         // LeerArchivoTexto.leerRegistros01();
-        // LeerArchivoTexto.leerRegistros02();
+        //LeerArchivoTexto.leerRegistros02();
         LeerArchivoTexto.leerRegistros03();
         // LeerArchivoTexto.leerRegistros04();
 
